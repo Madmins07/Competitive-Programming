9edc54c5dffa5b0daf16283af7536d32d26885e5
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-05-18 16:42:18 +0530

    Create 1325A.cpp

---
+++ codeforces/1325A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main() {
+    fastread();
+    int t,x;
+    cin >> t;
+    while(t--){
+        cin>>x;
+        cout<<1<<" "<<x-1<<endl;
+    }
+    return 0;
+}
