a6a038c23835c94b962c00a708a4726375940bf6
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-05-18 19:45:08 +0530

    Create 959A.cpp

---
+++ codeforces/959A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+ 
+int main()
+{
+    fastread();
+    int n;
+    cin>>n;
+    if(n%2!=0){
+        cout<<"Ehab"<<endl;
+    }else{
+        cout<<"Mahmoud"<<endl;
+    }
+    return 0;
+}
