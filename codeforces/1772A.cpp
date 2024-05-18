e53644b653bfc3c45f1f20f2ef98a0eae4a97966
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-05-18 20:02:43 +0530

    Create 1772A.cpp

---
+++ codeforces/1772A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+ 
+int main()
+{
+    fastread();
+    int t,a,b;
+    cin>>t;
+    while(t--){
+    cin>>a>>b;
+    cout<<a+b<<endl;}
+    return 0;
+}
