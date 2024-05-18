fc864de03c63b36ad6a5def3e24770c07b5178b5
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-05-18 15:00:27 +0530

    Create 1372A.cpp

---
+++ codeforces/1372A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+ 
+int main() {
+    fastread();
+    int t,n;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cout<<1<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
