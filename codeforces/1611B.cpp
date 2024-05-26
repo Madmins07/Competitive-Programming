4e4dfdc63241fbc6b96914862c1369e7d2c29837
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-05-26 16:08:53 +0530

    Update 1611B.cpp

--- codeforces/1611B.cpp
+++ codeforces/1611B.cpp
@@ -1,13 +1,20 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
 #include <cstdio>
 
-int main(){
-
-    long t; scanf("%ld", &t);
-    while(t--){
-        long a, b; scanf("%ld %ld", &a, &b);
+int main() {
+    fastread();
+    long t;
+    cin >> t;
+    while (t--) {
+        long a, b;
+        cin >> a >> b;
         long res = (a < b ? a : b);
-        res = (res < (a + b) / 4) ? res : (a + b)/4;
-        printf("%ld\n", res);
+        res = (res < (a + b) / 4) ? res : (a + b) / 4;
+        cout << res << endl;
     }
-
+    return 0;
 }
