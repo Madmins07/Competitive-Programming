9080c197cd3f3774fa48010158e039c4cc6bc042
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-07-01 19:51:41 +0530

    Create 1768A.cpp

---
+++ codeforces/1768A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main() {
+    fastread();
+    int t,k;
+    cin >> t;
+    while (t--) {
+        cin>>k;
+        cout<<k-1<<endl;
+    }
+    return 0;
+}
