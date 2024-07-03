ad7d45441c47a6c920d48b86c532cd12a9c8a2ce
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-07-03 19:33:24 +0530

    Create 1285A.cpp

---
+++ codeforces/1285A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main() {
+    fastread();
+    int t;
+    cin>>t;
+    char s[t];
+    cin>>s;
+    cout<<t+1<<endl;
+}
