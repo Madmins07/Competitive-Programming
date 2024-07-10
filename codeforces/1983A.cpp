24c642bfad855cbdd4356702c92ffa38d2d021df
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-07-10 19:26:06 +0530

    Create 1983A.cpp

---
+++ codeforces/1983A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main() {
+    fastread();
+    int t, n, m;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
