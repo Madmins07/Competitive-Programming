d231d88b88e30341b42ef789749c5e784b2146cf
Madhav Agarwal <152009368+Madmins07@users.noreply.github.com>
2024-09-08 17:13:10 +0530

    Create 2010B.cpp

---
+++ codeforces/2010B.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main() {
+    fastread();
+    int a, b;
+    cin >> a >> b;
+    cout << 6 - a - b << endl;
+    return 0;
+}
